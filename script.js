fe534cb9e7cc2038576a9e20ee92541830d0d6f5
Piotr Cybulski <piotrekkc2@gmail.com>
2018-01-12 14:52:18 +0100

    Add basic code to index and script files

---
+++ script.js
@@ -0,0 +1,10 @@
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur ="triceratops";
+
+var dinosaurUpperCased = dinosaur.toUpperCase();
+
+console.log(dinosaurUpperCased);
+
+var textCorrect = text.replace('Velociraptor', dinosaurUpperCased);
+
+console.log(textCorrect);
