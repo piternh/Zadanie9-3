83b98cd0adfa676522d1e133e42dab6e44a0f7be
Piotr Cybulski <piotrekkc2@gmail.com>
2018-01-13 11:39:14 +0100

    Edit code in script.js

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ console.log(dinosaurUpperCased);
 
 var textCorrect = text.replace('Velociraptor', dinosaurUpperCased);
 
-console.log(textCorrect);
+console.log(textCorrect.length/2);
+
+console.log(textCorrect.slice(0, 72));
